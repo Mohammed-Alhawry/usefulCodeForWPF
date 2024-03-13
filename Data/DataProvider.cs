32d2549e750754af474bd82f4dd1378587ed965d
Mohammed Alhawry <alhawryprogrammer@gmail.com>
2024-03-13 13:47:39 +0300

    Fixed some colors issues

--- Data/DataProvider.cs
+++ Data/DataProvider.cs
@@ -37,6 +37,34 @@ public static class DataProvider
                 ShortName = "ALKsd",
                 FullName = "laskdfsdflsdkf",
                 Price = 112112128.1m
+            },
+            new Material
+            {
+                Id = 1,
+                ShortName = "ALK",
+                FullName = "laskdflsdkf",
+                Price = 1928.1m
+            }
+            ,new Material
+            {
+                Id = 2,
+                ShortName = "SSsdfsdfK",
+                FullName = "laskdflsdsdfasdfasdfkf",
+                Price = 1928.1m
+            }
+            ,new Material
+            {
+                Id = 3,
+                ShortName = "AL",
+                FullName = "lasskdflsdkf",
+                Price = 1928.1m
+            },
+            new Material
+            {
+                Id = 4,
+                ShortName = "ALKsd",
+                FullName = "laskdfsdflsdkf",
+                Price = 112112128.1m
             }
         };
     }
