af89e7bc551ef98702a6c00aa09c2353e8f615a6
Mohammed Alhawry <alhawryprogrammer@gmail.com>
2024-03-26 14:10:33 +0300

    Finished the deletion operation of the Materials

--- ViewModels/MaterialsViewModel.cs
+++ ViewModels/MaterialsViewModel.cs
@@ -1,7 +1,10 @@
-﻿using ExperimentalThingsUsingWPF.Data;
+﻿using ControlzEx.Standard;
+using ExperimentalThingsUsingWPF.Data;
 using ExperimentalThingsUsingWPF.HelperClasses;
 using ExperimentalThingsUsingWPF.Windows;
 using System.Collections.ObjectModel;
+using System.Windows;
+
 
 namespace ExperimentalThingsUsingWPF.ViewModels;
 
@@ -9,19 +12,34 @@ public class MaterialsViewModel : ViewModelBase
 {
     public ObservableCollection<MaterialObjectViewModel> Materials { get; set; }
     public MaterialObjectViewModel SelectedMaterial { get; set; }
-    
+
     public DelegateCommand EditMaterialCommand { get; set; }
     public DelegateCommand OpenAddMaterialWindowCommand { get; set; }
-    
+    public DelegateCommand OpenDeleteConfirmationDialogCommand { get; set; }
+
 
     public MaterialsViewModel()
     {
         OpenAddMaterialWindowCommand = new DelegateCommand(OpenAddMaterialWindow);
         EditMaterialCommand = new DelegateCommand(OpenEditMaterialWindow);
-    
+        OpenDeleteConfirmationDialogCommand = new DelegateCommand(OpenDeleteConfirmationDialog);
     }
 
-    
+    private void OpenDeleteConfirmationDialog(object obj)
+    {
+        if (SelectedMaterial is not null)
+        {
+            var messageBoxText = "Are you sure that you want to delete?";
+            var caption = "Delete Material";
+            var messageBoxButton = MessageBoxButton.YesNo;
+            var messageBoxIcon = MessageBoxImage.Warning;
+            var yesOrNo = MessageBox.Show(messageBoxText, caption, messageBoxButton, messageBoxIcon);
+            if (yesOrNo == MessageBoxResult.Yes) 
+                Materials.Remove(SelectedMaterial);
+
+        }
+    }
+
     private void OpenAddMaterialWindow(object obj)
     {
         var addMaterialWindow = new AddMaterialWindow(new AddMaterialViewModel());
@@ -37,9 +55,10 @@ public class MaterialsViewModel : ViewModelBase
 
     public async Task OnLoadedAsync()
     {
+        
         await Task.Run(() => Materials = MaterialsTransformer.TransformToViewModels(DataProvider.GetMaterials()));
         OnPropertyChanged(nameof(Materials));
     }
 
-    
+
 }
